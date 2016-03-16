47f6f7017832c1f24f92b675d55956fbfd3d0c06
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-16 12:17:15 +0100

    #4: Fixed some of the Dockerfile path bugs.

--- resolver/docker/build_docker.sh
+++ resolver/docker/build_docker.sh
@@ -9,9 +9,6 @@ cd $TMP_DIR
 git clone https://github.com/CentOS/CentOS-Dockerfiles.git
 
 
-ln -s $TMP_DIR/CentOS-Dockerfiles/postgres/centos7 postgres
-
-
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u72-b14/jdk-7u72-linux-x64.tar.gz"
 
 wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.8/bin/apache-tomcat-7.0.8.tar.gz
