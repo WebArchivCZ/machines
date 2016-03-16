3e6cd2ff603d37c89beed8c427c67e86c673d9e1
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-16 12:08:40 +0100

    #4: Added some of the parts of the build scripts.

--- resolver/docker/build_docker.sh
+++ resolver/docker/build_docker.sh
@@ -1,17 +1,24 @@
 #! /usr/bin/env bash
 
 WORK_DIR=`pwd`
-cd /tmp
+TMP_DIR="$WORK_DIR/tmp"
 
-git clone https://github.com/kirillF/centos-tomcat.git
+mkdir $TMP_DIR
+cd $TMP_DIR
+
 git clone https://github.com/CentOS/CentOS-Dockerfiles.git
 
+
+ln -s $TMP_DIR/CentOS-Dockerfiles/postgres/centos7 postgres
+
+
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u72-b14/jdk-7u72-linux-x64.tar.gz"
+
+wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.8/bin/apache-tomcat-7.0.8.tar.gz
+
+
+cp "$WORK_DIR/Dockerfile" .
+docker build --rm=true -t urn_nbn_resolver .
+
 cd $WORK_DIR
-
-ln -s /tmp/centos-tomcat tomcat
-ln -s /tmp/CentOS-Dockerfiles/postgres/centos7 postgres
-
-# rm tomcat
-# rm postgres
-# rm -r /tmp/centos-tomcat
-# rm -r /tmp/CentOS-Dockerfiles
+# rm -r $TMP_DIR
